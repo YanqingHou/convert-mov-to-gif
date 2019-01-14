1c92b8b76b005186f283189792b688940fec0e68
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-01-13 23:34:16 -0800

    中文命名

--- Converter.rb
+++ Converter.rb
@@ -1,31 +1,34 @@
+# encoding: utf-8
+
 APP_ROOT = File.dirname(__FILE__)
 
+@@mov文件名 = "1214_田字格"
+
 class Converter
+  @@mov视频大小 = "1032x542" # 查看mov文件属性
   # Default path to ffmpeg installed by homebrew
   @@ffmpeg   = File.join('/', 'usr', 'local', 'bin', 'ffmpeg')
   # Default path to gifsicle installed by homebrew
   @@gifsicle = File.join('/', 'usr', 'local', 'bin', 'gifsicle')
-  # Tell ffmpeg the max-width and max-height
-  @@max_size = ["1280x1024", "800x640", "640x512", "600x480", "480x384", "300x240"][2]
-  @@pixel_format = "rgb8"
+
+  @@像素格式 = "rgb8"
   # Tell ffmpeg to reduce the frame rate from to 30
-  @@frames = 30
+  @@帧数 = 30
 
   # Tell gifsicle to delay 30ms between each gif (in ms)
-  @@delay = 3
+  @@延时 = 3
   # Requests that gifsicle use the slowest/most file-size optimization
-  @@optimize = 3
+  @@优化 = 3
   
-  def initialize(input, output)
-    shell_command = %{#{@@ffmpeg} -i #{input} -s #{@@max_size} -pix_fmt #{@@pixel_format} -r #{@@frames} -f gif - | #{@@gifsicle} --optimize=#{@@optimize} --delay=#{@@delay} > #{output}}
-    # puts command
+  def initialize(输入, 输出)
+    命令 = %{#{@@ffmpeg} -i #{输入} -s #{@@mov视频大小} -pix_fmt #{@@像素格式} -r #{@@帧数} -f gif - | #{@@gifsicle} --optimize=#{@@优化} --delay=#{@@延时} > #{输出}}
     
-    %x[ #{shell_command} ]
+    %x[ #{命令} ]
   end  
 end
 
 # Be careful not to use spa ces.
-input  = File.join(APP_ROOT, 'media', 'Circle.mov')
-output = File.join(APP_ROOT, 'media', 'Circle.gif')
+输入 = File.join(APP_ROOT, 'media', @@mov文件名 + '.mov')
+输出 = File.join(APP_ROOT, 'media', @@mov文件名 + '.gif')
 
-Converter.new(input,output)
+Converter.new(输入,输出)
